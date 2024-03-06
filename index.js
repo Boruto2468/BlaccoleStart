4c4e73aa5b2337c7f9f0cb177db5c125f1d2c344
Dhajini <frankarimoro@gmail.com>
2024-03-06 17:50:27 +0100

    started replicating  desktopmetal

---
+++ index.js
@@ -0,0 +1,54 @@
+// keep track of previous scroll position
+let prevScrollPos = window.scrollY;
+
+window.addEventListener('scroll', function() {
+  // current scroll position
+  const currentScrollPos = window.scrollY;
+
+  if (prevScrollPos > currentScrollPos) {
+    document.getElementById("nav-bar").style.top = "0px"; // Show navbar
+} else {
+    document.getElementById("nav-bar").style.top = "-150px"; // Hide navbar
+}
+
+  // update previous scroll position
+  prevScrollPos = currentScrollPos;
+});
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+let buttonplus = document.getElementById('toggleButton');
+
+        buttonplus.addEventListener('click', function() {
+            this.classList.toggle('negative');
+            this.textContent = this.classList.contains('negative') ? '-' : 'X';
+        });
