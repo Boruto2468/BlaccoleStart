8a87ac3362985036b6b2f8b2dedcce98b6583078
Dhajini <frankarimoro@gmail.com>
2024-03-31 11:36:16 +0100

    Updated video slider

--- index.js
+++ index.js
@@ -14,20 +14,106 @@ window.addEventListener('scroll', function() {
   // update previous scroll position
   prevScrollPos = currentScrollPos;
 });
-
-const slider = document.querySelector('.slider');
-const slides = document.querySelectorAll('.slide');
-
-let currentSlide = 0;
-console.log(currentSlide);
-console.log(typeof currentSlide);
-
-
-
-function nextSlide() {
-  slides[currentSlide].classList.remove('active');
-  currentSlide = (currentSlide + 1) % slides.length;
-  slides[currentSlide].classList.add('active');
+//Side Panel
+function openNav() {
+  document.getElementById("mySidepanel").style.width = "250px";
 }
 
-setInterval(nextSlide, 3000);
+/* Set the width of the sidebar to 0 (hide it) */
+function closeNav() {
+  document.getElementById("mySidepanel").style.width = "0";
+}
+
+// video code
+
+const slideContainer = document.querySelector('.container');
+const slide = document.querySelector('.slides');
+const nextBtn = document.getElementById('next-btn');
+const prevBtn = document.getElementById('prev-btn');
+const interval = 3000;
+
+let slides = document.querySelectorAll('.slide');
+let index = 1;
+let slideId;
+
+const firstClone = slides[0].cloneNode(true);
+const lastClone = slides[slides.length - 1].cloneNode(true);
+
+firstClone.id = 'first-clone';
+lastClone.id = 'last-clone';
+
+slide.append(firstClone);
+slide.prepend(lastClone);
+
+const slideWidth = slides[index].clientWidth;
+
+slide.style.transform = `translateX(${-slideWidth * index}px)`;
+
+console.log(slides);
+
+const startSlide = () => {
+  slideId = setInterval(() => {
+    moveToNextSlide();
+  }, interval);
+};
+
+const getSlides = () => document.querySelectorAll('.slide');
+
+slide.addEventListener('transitionend', () => {
+  slides = getSlides();
+  if (slides[index].id === firstClone.id) {
+    slide.style.transition = 'none';
+    index = 1;
+    slide.style.transform = `translateX(${-slideWidth * index}px)`;
+  }
+
+  if (slides[index].id === lastClone.id) {
+    slide.style.transition = 'none';
+    index = slides.length - 2;
+    slide.style.transform = `translateX(${-slideWidth * index}px)`;
+  }
+});
+
+const moveToNextSlide = () => {
+  slides = getSlides();
+  if (index >= slides.length - 1) return;
+  index++;
+  slide.style.transition = '.7s ease-out';
+  slide.style.transform = `translateX(${-slideWidth * index}px)`;
+};
+
+const moveToPreviousSlide = () => {
+  if (index <= 0) return;
+  index--;
+  slide.style.transition = '.7s ease-out';
+  slide.style.transform = `translateX(${-slideWidth * index}px)`;
+};
+
+slideContainer.addEventListener('mouseenter', () => {
+  clearInterval(slideId);
+});
+
+slideContainer.addEventListener('mouseleave', startSlide);
+nextBtn.addEventListener('click', moveToNextSlide);
+prevBtn.addEventListener('click', moveToPreviousSlide);
+
+startSlide();
+
+
+/* const autoSlideshow = () => {
+  const slides = document.querySelectorAll('.slide video')
+  const slideDelay = 6000
+  let currentSlide = 0
+
+  slides[currentSlide].classList.add('active')
+
+  const nextSlide = () => {
+      slides[currentSlide].classList.remove('active')
+      currentSlide = (currentSlide + 1) % slides.length
+      slides[currentSlide].classList.add('active')
+  }
+  console.log(nextSlide)
+  setInterval(nextSlide, slideDelay)
+}
+autoSlideshow()
+console.log(autoSlideshow); */
